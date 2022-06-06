3655b7bbcc72e4f8e456e94406b2284087f96edd
Shahila Mafraz <fathima.shahila371@gmail.com>
2022-06-06 16:06:16 +0800

    useReducer practice improved

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -3,8 +3,25 @@ import Modal from './Modal';
 import { data } from '../../../data';
 // reducer function
 
-const reducer = (state, action) =>{}
-  const defaultState = {
+const reducer = (state, action) =>{
+  
+  if(action.type==='ADD_ITEM'){
+    const newPeople = [...state.people, action.payload]
+    return {
+      ...state,
+      people: newPeople,
+      isModalOpen: true,
+      modalContent: 'item added'
+    }
+  }
+  if(action.type==='NO_VALUE'){
+    return {...state, isModalOpen: true, modalContent: 'Please enter a value'}
+  }
+ 
+  throw new Error('no matching action type')
+}
+  
+const defaultState = {
     people: [],
     isModalOpen: false,
     modalContent: '',
@@ -19,10 +36,16 @@ const Index = () => {
   const handleSubmit = (e) =>{
     e.preventDefault()
     if(name){
-      
+      const newItem = {id: new Date().getTime().toString(),name}
+      dispatch({type: 'ADD_ITEM', payload: newItem})
+      setName('')
     }else{
-      
+      dispatch({type: 'NO_VALUE'})
     }
+  }
+
+  const closeModal = ()=>{
+    dispatch({type: 'CLOSE_MODAL'})
   }
 
   return(
